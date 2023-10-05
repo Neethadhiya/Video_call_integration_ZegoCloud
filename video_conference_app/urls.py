2c8ff5ca478b40c95fc9e85c6f0cabd397a8fead
unknown <neethadhiya@gmail.com>
2023-10-05 13:34:11 +0530

    video conference completed

--- video_conference_app/urls.py
+++ video_conference_app/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('register/',views.register,name='register')
-    
+    path('register/',views.register,name='register'),
+    path('login_view/',views.login_view,name='login_view'),
+    path('dashboard/',views.dashboard,name='dashboard'),
+    path('meeting/',views.video_call,name='meeting'),
+    path('join_room/',views.join_room,name='join_room'),
+    path('logout/',views.logout_view,name='logout'),
 ]
